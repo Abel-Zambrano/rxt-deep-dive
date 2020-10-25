e3df924585e952026af8e637979be0adf28f9e09
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-25 12:12:04 -0700

    added focus to Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,15 +2,31 @@ import React, { Component, Fragment } from 'react';
 
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef(); // Adds focus to last person element pt1
+  }
+
+  componentDidMount() {  
+    this.inputElementRef.current.focus(); // Adds focus to last person element pt2
+
+  } 
+
   render () {
-    console.log('[Person.js] rendering...');
+    console.log('[Person.js] rendering...');  
+
   return (
     <Fragment> 
       <p onClick={this.props.click}>
         I'm {this.props.name} and I am {this.props.age} years old!
       </p>
       <p>{this.props.children}</p>
-      <input type="text" onChange={this.props.changed} value={this.props.name} />
+      <input 
+        type="text"
+        ref={this.inputElementRef} // Adds focus to last person element pt3
+        onChange={this.props.changed} 
+        value={this.props.name} 
+      />
     </Fragment>
   );
 };
