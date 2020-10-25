fd0b7fd684c55b0905717860acf7655ce0b9906f
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-25 11:35:08 -0700

    update to nameChangedHandler method

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -63,7 +64,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    // Method ensures we are using previous state that depends on old state
+    this.setState((prevState, props) => {
+      return { 
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1 };
+    });
   };
 
   deletePersonHandler = personIndex => {
@@ -91,7 +97,7 @@ class App extends Component {
         />
       );
     }
-
+  
     return (
       <div className={classes.App}>
         <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
